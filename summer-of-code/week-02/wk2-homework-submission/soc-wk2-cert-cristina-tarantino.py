f6ea1a749f3092472de7b8faddc08e772acd2f60
Cristina Tarantino <cristina@tarantino.io>
2018-07-24 16:23:15 +0100

    added todays notes

--- summer-of-code/week-02/wk2-homework-submission/soc-wk2-cert-cristina-tarantino.py
+++ summer-of-code/week-02/wk2-homework-submission/soc-wk2-cert-cristina-tarantino.py
@@ -32,3 +32,13 @@ f.closed
 # print(count_letters(raw))
 for key, value in count_letters(read_data).items():
     print("Number of letter " + key + ": " + str(value))
+
+# 2. numbers to letters
+for i in range(65, 65+2*26)
+    print(i, " stand for ", chr(i))
+
+# fix the code to only show A-Z and a-z with a function
+
+# 3. make a function that asks the user for a message, and turns it into a list of numbers.
+
+# 4. Caesar cypher
