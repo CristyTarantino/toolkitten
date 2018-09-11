ee94ff186df173f46d72725ca36ff2d727ecaea5
Cristina Tarantino <cristina@tarantino.io>
2018-09-11 10:02:37 +0100

    added some comments to solidity code

--- summer-of-code/week-09/wk9-homework-submissions/1mwtt_coin_storage.sol
+++ summer-of-code/week-09/wk9-homework-submissions/1mwtt_coin_storage.sol
@@ -1,4 +1,10 @@
 pragma solidity ^0.4.0;
+// the source code is written for Solidity version 0.4.0 or anything newer that does not break functionality
+// This is to ensure that the contract is not compilable with a new (breaking) compiler version,
+// where it could behave differently.
+
+// A contract in the sense of Solidity is a collection of code (its functions) and data (its state)
+// that resides at a specific address on the Ethereum blockchain.
 
 contract SimpleStorage {
     uint storageData;
